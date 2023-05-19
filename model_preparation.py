3440fd8eccd7fc1e6d9829f5acd4786db0052ba4
PetrGavrilin <gapeal@mail.ru>
2023-05-19 19:36:55 +0300

    MyFirstCommit

---
+++ model_preparation.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from joblib import dump, load
+from sklearn.pipeline import Pipeline#импортирую бибилотеку трубы
+
+
+X_train = pd.read_csv('X_train.csv')
+y_train = pd.read_csv('y_train.csv')
+
+
+pipe = Pipeline([ 
+    ('KNeighborsClassifier', KNeighborsClassifier(n_neighbors=2)) # назначаем в качестве модели knn
+])
+
+pipe = pipe.fit(X_train, y_train)
+
+dump(pipe, 'knn.joblib') #задаём название для модели
+
+pipe_knn = load('knn.joblib') #назначаем переменную и сохраняем
+print('model_preparation исполнен')
---
+++ model_testing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.metrics import recall_score,roc_auc_score,f1_score
+from joblib import load
+
+knn = load('knn.joblib') 
+
+X_test = pd.read_csv('X_test.csv')
+y_test = pd.read_csv('y_test.csv')
+print(y_test)
+
+def three_score(model_pipe, X, y): #оценка модели отдельной функцией, 3 метрики, условие выполнено
+    """Расчет коэффициента f1, roc_auc, recall 
+    
+    Параметры:
+    ===========
+    model_pipe: модель или pipeline
+    X: признаки
+    y: истинные значения
+    """
+    y_model = model_pipe.predict(X)
+    return f1_score(y, y_model), roc_auc_score(y,y_model),recall_score(y,y_model)
+y_model = knn.predict(X_test)
+print(f1_score( X_test, y_test)) #Classification metrics can't handle a mix of continuous-multioutput and multiclass-multioutput targets
+
+
+#print('результаты тестов',three_score(knn, X_test, y_test))#multiclass-multioutput is not supported
+print('pipeline исполнен успешно')

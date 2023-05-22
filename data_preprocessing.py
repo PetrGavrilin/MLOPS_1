a4ebc853384ad72e10f2e0f879c135e6a66f00e7
Petr <114101583+PetrGavrilin@users.noreply.github.com>
2023-05-22 21:47:29 +0300

    Update data_preprocessing.py

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -11,7 +11,7 @@ df['voice_mail_plan'] = df['voice_mail_plan'].map({'yes': 1, 'no': 0})
 df['churn'] = df['churn'].map({'yes': 1, 'no': 0})
 df['area_code'] = df['area_code'].map({'area_code_415': 1, 'area_code_408': 0,'area_code_510':3})
 
-X_train, X_test, y_train, y_test = train_test_split(df.drop(['churn'],axis=1), df['churn'].ravel(), stratify=df['churn'], test_size=0.2,random_state=0) 
+X_train, X_test, y_train, y_test = train_test_split(df.drop(['churn'],axis=1), df['churn'].values.ravel(), stratify=df['churn'], test_size=0.2,random_state=0) 
 
 standart_pipe = Pipeline([
     ('imputer', SimpleImputer(strategy='mean')),

8fd9acdee7126fbaf7b9f1848b46036e9b275d73
Petr <114101583+PetrGavrilin@users.noreply.github.com>
2023-05-23 21:55:08 +0300

    Update model_testing.py

--- model_testing.py
+++ model_testing.py
@@ -26,7 +26,7 @@ def three_score(model_pipe, X, y): #оценка модели отдельной
 
 y_model = knn.predict(X_test)
 print(np.unique(y_model, return_counts=True)
-#print(f1_score( y_test, y_model)) #Classification metrics can't handle a mix of continuous-multioutput and multiclass-multioutput targets
+print(f1_score( y_test, y_model)) #Classification metrics can't handle a mix of continuous-multioutput and multiclass-multioutput targets
 
 
 #print('результаты тестов',three_score(knn, X_test, y_test))#multiclass-multioutput is not supported

2a307d58fe1563e177aa820dcabdbb279e7cb747
Petr <114101583+PetrGavrilin@users.noreply.github.com>
2023-05-23 21:41:29 +0300

    Update model_testing.py

--- model_testing.py
+++ model_testing.py
@@ -21,7 +21,7 @@ def three_score(model_pipe, X, y): #оценка модели отдельной
     y_model = model_pipe.predict(X)
     return f1_score(y, np.round(y_model), average='weighted'), roc_auc_score(y,np.round(y_model), average='weighted'),recall_score(y,np.round(y_model), average='weighted')
 y_model = knn.predict(X_test)
-print(y_model.value_counts())
+print(np.unique(y_model, return_counts=True)
 print(f1_score( y_test, y_model)) #Classification metrics can't handle a mix of continuous-multioutput and multiclass-multioutput targets
 
 

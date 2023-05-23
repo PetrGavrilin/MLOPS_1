06604daee5805bb8eff14557ead3d23d94ad3621
Petr <114101583+PetrGavrilin@users.noreply.github.com>
2023-05-23 21:37:05 +0300

    Update model_testing.py

--- model_testing.py
+++ model_testing.py
@@ -25,5 +25,5 @@ print(y_model.value_counts())
 print(f1_score( y_test, y_model)) #Classification metrics can't handle a mix of continuous-multioutput and multiclass-multioutput targets
 
 
-#print('результаты тестов',three_score(knn, X_test, y_test))#multiclass-multioutput is not supported
+print('результаты тестов',three_score(knn, X_test, y_test))#multiclass-multioutput is not supported
 print('pipeline исполнен успешно')
